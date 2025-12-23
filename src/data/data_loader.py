1d7f8737673a41535b36b93f4c1bc095826ad188
Azarias Galama <azariasabera@gmail.com>
2025-12-23 08:42:04 +0200

    Add TFDS download and dataset loading utilities

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,6 +1,6 @@
 # src/data/data_loader.py
 
-from typing import Tuple, List
+from typing import Tuple
 from omegaconf import DictConfig
 import tensorflow_datasets as tfds
 import tensorflow as tf
@@ -20,7 +20,7 @@ def download_dataset(config: DictConfig) -> None:
                 - dataset.root_dir (str or Path)
     """
     tfds.load(
-        config.dataset.dataset_name,
+        name=config.dataset.dataset_name,
         data_dir=config.dataset.root_dir,
         split=["train", "validation", "test"],
         with_info=False,
@@ -32,33 +32,41 @@ def load_datasets( config: DictConfig) -> Tuple[tf.data.Dataset, tf.data.Dataset
     """
     Load train, validation, and test datasets from TensorFlow Datasets.
 
-    Assumes the dataset has already been downloaded and prepared.
-    If not found, raises a runtime error telling to download the dataset first.
+    Each dataset yields tuples (audio, label) because `as_supervised=True`:
+        - audio: tf.Tensor of shape (samples,) representing raw waveform
+        - label: tf.Tensor scalar containing class index
 
     Args:
         config (DictConfig): Hydra configuration containing dataset parameters.
+            Expected fields:
+                - dataset.dataset_name (str)
+                - dataset.root_dir (str or Path)
 
     Returns:
-        Tuple[tf.data.Dataset, tf.data.Dataset, tf.data.Dataset]:
-            Train, validation, and test datasets.
+        Tuple containing:
+            - ds_train (tf.data.Dataset): Training dataset
+            - ds_val (tf.data.Dataset): Validation dataset
+            - ds_test (tf.data.Dataset): Test dataset
+            - ds_info (tfds.core.DatasetInfo): Metadata for the dataset, including class labels
 
     Raises:
         RuntimeError: If the dataset is not found locally.
     """
     try:
-        ds_train, ds_val, ds_test = tfds.load(
+        (ds_train, ds_val, ds_test), ds_info = tfds.load(
             name=config.dataset.dataset_name,
             data_dir=config.dataset.root_dir,
             split=["train", "validation", "test"],
             as_supervised=True,
             shuffle_files=True,
+            with_info=True,
             download=False,
         )
-        return ds_train, ds_val, ds_test
+        return ds_train, ds_val, ds_test, ds_info
 
     except (AssertionError): # tsds raises AssertionError
         raise RuntimeError(
             f"Dataset '{config.dataset.dataset_name}' was not found in "
-            f"{config.dataset.root_dir}."
+            f"{config.dataset.root_dir}. "
             "Please run the dataset download step first!"
         ) from None
